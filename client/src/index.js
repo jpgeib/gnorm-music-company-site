a14d564893f4759669bd1c5702710eee257203af
James Geib <jpgeib2@gmail.com>
2020-10-06 16:43:13 -0400

    added Router tags in main index.js, deleted unnecessary code in App container, and added Home folder structure in pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BroswerRouter as Router } from "react-router-dom";
+
 import App from './containers/App';
 
-ReactDOM.render( <App />, document.getElementById('root'));
+ReactDOM.render( 
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root'));
