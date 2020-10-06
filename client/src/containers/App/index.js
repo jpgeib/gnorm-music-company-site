2a8b3f1f9b27728de749a08775d5aa82489279ca
James Geib <jpgeib2@gmail.com>
2020-10-06 16:58:26 -0400

    Set up Routes for each page in App container, fixed typo in main index.js

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { Route } from "react-router-dom";
+import Home from "../../pages/Home";
+import LiveMusic from "../../pages/LiveMusic";
+import Instruction from "../../pages/Instruction";
+import ProGear from "../../pages/ProGear";
+import Shows from "../../pages/Shows";
+import Contact from "../../pages/Contact";
 
 class App extends Component {
   
   render() {
     return (
       <>
-        
+        <Route exact path="/" component={Home} />
+        <Route exact path="/livemusic" component={LiveMusic} />
+        <Route exact path="/instruction" component={Instruction} />
+        <Route exact path="/progear" component={ProGear} />
+        <Route exact path="/shows" component={Shows} />
+        <Route exact path="/contact" component={Contact} />
       </>
     )
   }
