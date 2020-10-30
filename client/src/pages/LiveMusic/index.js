497b4e968247e7a33839b990bcac4928ce93f071
James Geib <jpgeib2@gmail.com>
2020-10-30 18:41:03 -0400

    constructed initial LiveMusic page, using a placeholder image for ShellShockt entry temporarily

--- client/src/pages/LiveMusic/index.js
+++ client/src/pages/LiveMusic/index.js
@@ -3,6 +3,7 @@ import { Grid, Image, Header } from "semantic-ui-react";
 import OutsideChance from "../../assets/images/336_OutsideChance1.JPG";
 import DNR from "../../assets/images/DNR-group.jpg";
 import Federation from "../../assets/images/fedpb.jpg";
+import Placeholder from "../../assets/images/Dethklokband.png";
 
 import "./style.css";
 
@@ -18,18 +19,22 @@ class LiveMusic extends Component {
                 <Grid id="live-container">
                     <Grid.Row>
                         <Grid.Column width={8}>
-
+                            <Image size="medium" src={OutsideChance} />
+                            <Header id="oc-header" as="h3">Outside Chance</Header>
                         </Grid.Column>
                         <Grid.Column width={8}>
-
+                            <Image size="medium" src={DNR} />
+                            <Header id="dnr-header" as="h3">DNR</Header>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
                         <Grid.Column width={8}>
-
+                            <Image size="medium" src={Federation} />
+                            <Header id="fed-header" as="h3">The Federation</Header>
                         </Grid.Column>
                         <Grid.Column width={8}>
-
+                            <Image size="medium" src={Placeholder} />
+                            <Header id="shell-header" as="h3">ShellShockt</Header>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
