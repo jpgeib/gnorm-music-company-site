f9c08f847637a2abb6f5f40a09e91c916b7adb98
James Geib <jpgeib2@gmail.com>
2020-10-30 15:56:45 -0400

    created HomeList component and rendered in Home page, adjusted Grid.Column widths andcss for home-container

---
+++ client/src/components/HomeList/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { List } from "semantic-ui-react";
+
+import "./style.css";
+
+export default class HomeList extends Component {
+    render() {
+        return (
+            <List>
+                <List.Item className="home-list-item">
+                    <List.Icon>🎸 </List.Icon>
+                    <List.Content>Guitar</List.Content>
+                </List.Item>
+                <List.Item className="home-list-item">
+                    <List.Icon>🎤</List.Icon>
+                    <List.Content>Vocals</List.Content>
+                </List.Item>
+                <List.Item className="home-list-item">
+                    <List.Icon>🎹</List.Icon>
+                    <List.Content>Keyboard</List.Content>
+                </List.Item>
+            </List>
+        );
+    }
+}
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Image, Header } from "semantic-ui-react";
+import HomeList from "../../components/HomeList";
 import GnormProfile from "../../assets/images/GnormProfile.jpg";
 
 import "./style.css";
@@ -15,21 +16,21 @@ class Home extends Component {
                 </Grid>
                 <Grid id="home-container">
                     <Grid.Row>
-                        <Grid.Column>
-                            <Grid>
+                        <Grid.Column width={8}>
+                            <Grid id="home-subcontainer">
                                 <Grid.Row>
-                                    <Grid.Column>
-                                        <Image src={GnormProfile} />
+                                    <Grid.Column width={16}>
+                                        <Image id="home-profile" size="medium" src={GnormProfile} />
                                     </Grid.Column>
                                 </Grid.Row>
                                 <Grid.Row>
-                                    <Grid.Column>
-
+                                    <Grid.Column width={16}>
+                                        <HomeList />
                                     </Grid.Column>
                                 </Grid.Row>
                             </Grid>
                         </Grid.Column>
-                        <Grid.Column>
+                        <Grid.Column width={8}>
 
                         </Grid.Column>
                     </Grid.Row>
