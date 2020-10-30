8b330bf7513304ef8d2261608a6007551b073cee
James Geib <jpgeib2@gmail.com>
2020-10-30 15:32:40 -0400

    created Grid structure in Home page and imported profile image for About Me section

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Image, Header } from "semantic-ui-react";
+import GnormProfile from "../../assets/images/GnormProfile.jpg";
 
 import "./style.css";
 
@@ -7,10 +8,31 @@ class Home extends Component {
     render() {
         return(
             <>
-                <Grid>
+                <Grid id="home-header-container">
                     <Grid.Column>
-                        <Header as="h1">Home</Header>
+                        <Header id="home-header" as="h1">Welcome to Gnorm Music - The G is Silent, I'm Not</Header>
                     </Grid.Column>
+                </Grid>
+                <Grid id="home-container">
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column>
+                                        <Image src={GnormProfile} />
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column>
+
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                        <Grid.Column>
+
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </>
         );
