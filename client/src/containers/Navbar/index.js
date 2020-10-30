db26f0218617c66d3d31d9d5174eb8de1b884acb
James Geib <jpgeib2@gmail.com>
2020-10-30 14:32:30 -0400

    adjusted structure/props of Navbar menu items

--- client/src/containers/Navbar/index.js
+++ client/src/containers/Navbar/index.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
+import "./style.css";
+
 export default class Navbar extends Component {
 
     state = {}
@@ -13,47 +15,67 @@ export default class Navbar extends Component {
         const { activeItem } = this.state;
 
         return (
-            <Menu pointing secondary>
+            <Menu id="navbar" secondary>
+                <Menu.Item
+                    as={Link}
+                    to="/"
+                    className="menu-item"
+                    name="home"
+                    active={activeItem === "home"}
+                    onClick={this.handleItemClick}
+                >
+                    About Me
+                </Menu.Item>
                 <Menu.Item
                     as={Link}
                     to="/livemusic"
+                    className="menu-item"
                     name="livemusic"
                     active={activeItem === "livemusic"}
-                    content="Live Music"
                     onClick={this.handleItemClick}
-                />
+                >
+                    Live Music
+                </Menu.Item>
                 <Menu.Item
                     as={Link}
                     to="/instruction"
+                    className="menu-item"
                     name="instruction"
                     active={activeItem === "instruction"}
-                    content="Instruction"
                     onClick={this.handleItemClick}
-                />
+                >
+                    Instruction
+                </Menu.Item>
                 <Menu.Item
                     as={Link}
                     to="/progear"
+                    className="menu-item"
                     name="progear"
                     active={activeItem === "progear"}
-                    content="Pro Gear"
                     onClick={this.handleItemClick}
-                />
+                >
+                    Pro Gear
+                </Menu.Item>
                 <Menu.Item
                     as={Link}
                     to="/shows"
+                    className="menu-item"
                     name="shows"
                     active={activeItem === "shows"}
-                    content="Shows"
                     onClick={this.handleItemClick}
-                />
+                >
+                    Shows
+                </Menu.Item>
                 <Menu.Item
                     as={Link}
                     to="/contact"
+                    className="menu-item"
                     name="contact"
                     active={activeItem === "contact"}
-                    content="Contact"
                     onClick={this.handleItemClick}
-                />
+                >
+                    Contact
+                </Menu.Item>
             </Menu>
         );
     }
