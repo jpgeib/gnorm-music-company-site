c9efdc5016cffcc80de92c2a0b894b6a12733078
James Geib <jpgeib2@gmail.com>
2020-10-30 18:23:07 -0400

    constructed initial Grid structure for LiveMusic page, imported necessary images

--- client/src/pages/LiveMusic/index.js
+++ client/src/pages/LiveMusic/index.js
@@ -1,16 +1,37 @@
 import React, { Component } from "react";
 import { Grid, Image, Header } from "semantic-ui-react";
+import OutsideChance from "../../assets/images/336_OutsideChance1.JPG";
+import DNR from "../../assets/images/DNR-group.jpg";
+import Federation from "../../assets/images/fedpb.jpg";
 
 import "./style.css";
 
 class LiveMusic extends Component {
     render() {
-        return(
+        return (
             <>
-                <Grid>
-                    <Grid.Column>
-                        <Header as="h1">LiveMusic</Header>
+                <Grid id="live-header-container">
+                    <Grid.Column width={16}>
+                        <Header id="live-header" as="h1">These are the bands I work with most. Between them, we cover everything from Heavy Metal to Frank Sinatra.</Header>
                     </Grid.Column>
+                </Grid>
+                <Grid id="live-container">
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </>
         );
