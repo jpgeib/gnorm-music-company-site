258245e4627ce0a351ad3a6c0aff8165c725b0db
James Geib <jpgeib2@gmail.com>
2020-10-31 13:29:47 -0400

    deleted Instruction Route, import, and folder structure

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -3,7 +3,6 @@ import { Route } from "react-router-dom";
 import Navbar from "../Navbar";
 import Home from "../../pages/Home";
 import LiveMusic from "../../pages/LiveMusic";
-import Instruction from "../../pages/Instruction";
 import ProGear from "../../pages/ProGear";
 import Shows from "../../pages/Shows";
 import Contact from "../../pages/Contact";
@@ -17,7 +16,6 @@ class App extends Component {
         <Navbar />
         <Route exact path="/" component={Home} />
         <Route exact path="/livemusic" component={LiveMusic} />
-        <Route exact path="/instruction" component={Instruction} />
         <Route exact path="/progear" component={ProGear} />
         <Route exact path="/shows" component={Shows} />
         <Route exact path="/contact" component={Contact} />
