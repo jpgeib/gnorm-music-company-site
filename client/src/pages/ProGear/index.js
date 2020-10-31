4abb5e7def4843b1d1e32022567bfb064f892d6a
James Geib <jpgeib2@gmail.com>
2020-10-31 13:41:53 -0400

    created initial Grid structure for ProGear page

--- client/src/pages/ProGear/index.js
+++ client/src/pages/ProGear/index.js
@@ -7,10 +7,33 @@ class ProGear extends Component {
     render() {
         return(
             <>
-                <Grid>
-                    <Grid.Column>
-                        <Header as="h1">ProGear</Header>
+                <Grid id="progear-header-container">
+                    <Grid.Column width={16}>
+                        <Header id="progear-header" as="h1">I am a dealer for selected pro gear that I actually use.</Header>
                     </Grid.Column>
+                    <Grid.Column width={16}>
+                        <Header id="progear-subheader" as="h3">As a working musician, quality, reliabilty, and responsive customer support are absolute requirements. All the lines I handle have those virtues.</Header>
+                    </Grid.Column>
+                </Grid>
+                <Grid id="progear-container">
+                    <Grid.Row>
+
+                    </Grid.Row>
+                    <Grid.Row>
+
+                    </Grid.Row>
+                    <Grid.Row>
+
+                    </Grid.Row>
+                    <Grid.Row>
+                        
+                    </Grid.Row>
+                    <Grid.Row>
+                        
+                    </Grid.Row>
+                    <Grid.Row>
+                        
+                    </Grid.Row>
                 </Grid>
             </>
         );
