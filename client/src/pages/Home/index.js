4fd13ee838301fea815e7da38a1a9abc739b14fd
James Geib <jpgeib2@gmail.com>
2020-11-17 20:28:00 -0500

    added placeholder text for bio section on Home Page

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Image, Header } from "semantic-ui-react";
 import HomeList from "../../components/HomeList";
-// import Footer from "../../components/Footer";
 import GnormProfile from "../../assets/images/GnormProfile.jpg";
 
 import "./style.css";
@@ -17,7 +16,7 @@ class Home extends Component {
                 </Grid>
                 <Grid id="home-container">
                     <Grid.Row>
-                        <Grid.Column width={8}>
+                        <Grid.Column width={5}>
                             <Grid id="home-subcontainer">
                                 <Grid.Row>
                                     <Grid.Column width={16}>
@@ -31,8 +30,39 @@ class Home extends Component {
                                 </Grid.Row>
                             </Grid>
                         </Grid.Column>
-                        <Grid.Column width={8}>
-
+                        <Grid.Column width={11}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                                            Expedita cupiditate temporibus dicta hic atque amet cum 
+                                            molestiae dolor praesentium quos obcaecati earum adipisci, 
+                                            eius, consequuntur suscipit ex illum sed modi.
+                                        </Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                                            Expedita cupiditate temporibus dicta hic atque amet cum 
+                                            molestiae dolor praesentium quos obcaecati earum adipisci, 
+                                            eius, consequuntur suscipit ex illum sed modi.
+                                        </Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                                            Expedita cupiditate temporibus dicta hic atque amet cum 
+                                            molestiae dolor praesentium quos obcaecati earum adipisci, 
+                                            eius, consequuntur suscipit ex illum sed modi.
+                                        </Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
