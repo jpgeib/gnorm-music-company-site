743d1d0590eb1e6bf3b2a472833c369fe4e8d97f
James Geib <jpgeib2@gmail.com>
2020-11-17 20:13:11 -0500

    added links to band subpages for each respective image in LiveMusic page

--- client/src/pages/LiveMusic/index.js
+++ client/src/pages/LiveMusic/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Image, Header } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 import OutsideChance from "../../assets/images/336_OutsideChance1.JPG";
 import DNR from "../../assets/images/DNR-group.jpg";
 import Federation from "../../assets/images/fedpb.jpg";
@@ -19,21 +20,21 @@ class LiveMusic extends Component {
                 <Grid id="live-container">
                     <Grid.Row>
                         <Grid.Column width={8}>
-                            <Image size="medium" src={OutsideChance} />
+                            <Image as={Link} to="/livemusic/outsidechance" size="medium" src={OutsideChance} />
                             <Header id="oc-header" as="h3">Outside Chance</Header>
                         </Grid.Column>
                         <Grid.Column width={8}>
-                            <Image size="medium" src={DNR} />
+                            <Image as={Link} to="/livemusic/dnr" size="medium" src={DNR} />
                             <Header id="dnr-header" as="h3">DNR</Header>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
                         <Grid.Column width={8}>
-                            <Image size="medium" src={Federation} />
+                            <Image as={Link} to="/livemusic/federation" size="medium" src={Federation} />
                             <Header id="fed-header" as="h3">The Federation</Header>
                         </Grid.Column>
                         <Grid.Column width={8}>
-                            <Image size="medium" src={Placeholder} />
+                            <Image as={Link} to="/livemusic/shellshockt" size="medium" src={Placeholder} />
                             <Header id="shell-header" as="h3">ShellShockt</Header>
                         </Grid.Column>
                     </Grid.Row>
