1fa72240c80eeeb62dc71e4d3bf0dbec4aab7618
James Geib <jpgeib2@gmail.com>
2021-01-20 20:16:53 -0500

    created initial Grid structure for Contact page, adjusted css for Footer component

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
-import { Grid, Header } from "semantic-ui-react";
+import { Grid, Header, Image } from "semantic-ui-react";
+import Telecast from "../../assets/images/img_gnfl.jpg";
 
 import "./style.css";
 
 class Contact extends Component {
     render() {
-        return(
+        return (
             <>
-                <Grid>
-                    <Grid.Column>
-                        <Header as="h1">Contact</Header>
-                    </Grid.Column>
+                <Grid id="contact-container">
+                    <Grid.Row>
+                        <Header id="contact-header" as="h1">Contact</Header>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image id="contact-image" src={Telecast} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Header as="h3">Gnorm (The 'G' is silent, I'm not...)</Header>
+                            <Header as="h3">Gnorm Music, LLC</Header>
+                            
+                        </Grid.Column>
+                    </Grid.Row>
+
                 </Grid>
             </>
         );
