c8aef20bf179f3b48cf3e891d7a3e0938d09b676
James Geib <jpgeib2@gmail.com>
2021-02-03 00:51:54 -0500

    created general introductory structure for ProGear page

--- client/src/pages/ProGear/index.js
+++ client/src/pages/ProGear/index.js
@@ -1,26 +1,43 @@
 import React, { Component } from "react";
-import { Grid, Header } from "semantic-ui-react";
+import { Grid, Header, Image } from "semantic-ui-react";
 import Guitars from "../../assets/images/img_guitars.jpg";
 import ProGearNavbar from "../../containers/ProGearNavbar";
+import { progearText } from "../../text";
 
 import "./style.css";
 
 class ProGear extends Component {
     render() {
-        return(
+        return (
             <>
                 {/* <Grid id="progear-header-container">
-                    <Grid.Column width={16}>
-                        <Header id="progear-header" as="h1">{progearHeader}</Header>
-                    </Grid.Column>
+                    
                     <Grid.Column width={16}>
                         <Header id="progear-subheader" as="h3">{progearSubheader}</Header>
                     </Grid.Column>
                 </Grid> */}
-                
+
                 <Grid id="progear-container">
                     <ProGearNavbar />
-            
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="medium" src={Guitars} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header id="progear-header" as="h2">Pro Gear</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header id="progear-text" as="h3">{progearText.main}</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </>
         );
--- client/src/text/index.js
+++ client/src/text/index.js
@@ -11,8 +11,7 @@ export const livemusicText = {
 //ProGear page text
 
 export const progearText = {
-    header: "I am a dealer for selected pro gear that I actually use.",
-    subheader: "As a working musician, quality, reliabilty, and responsive customer support are absolute requirements. All the lines I handle have those virtues.",
+    main: "I am a dealer for selected pro gear that I actually use. As a working musician, quality, reliabilty, and responsive customer support are absolute requirements. All the lines I handle have those virtues.",
     audix: "I've been using Audix mikes for about 6 years. I started with OM5's and OM3's for vocals and D3's for guitar cab miking. I've since added the D6 kick drum mike and SCX1 condensers to my live rig. I became a dealer about 2 years ago because the product has proved itself through countless gigs.",
     boomerang: "The Boomerang is a foot-operated real-time looper. I use it to lay down tracks for myself when I'm learning songs or developing parts for new compositions. It can also be used live as a digital delay with two speeds.",
     georgeLs: "George L's solderless cable is the pro standard for custom-wired pedal boards. They recently added solderless speaker cables to the line.",
