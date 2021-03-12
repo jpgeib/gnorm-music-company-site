dbdf280437e0fc8bb69b1f342aebb9f6c63af2d9
James Geib <jpgeib2@gmail.com>
2021-03-12 14:24:52 -0500

    added volume dial gifs to ProGearNavbar elements as bullet points

--- client/src/containers/ProGearNavbar/index.js
+++ client/src/containers/ProGearNavbar/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import { Menu } from "semantic-ui-react";
+import { Menu, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import VolumeDial from "../../assets/gifs/dial_bullet.gif";
 
 import "./style.css";
 
 export default class ProGearNavbar extends Component {
     state = {
-        // activeItem: "audix"
+        activeItem: ""
     }
 
     handleItemClick = (e, { name }) => this.setState({ activeItem: name });
@@ -23,42 +24,60 @@ export default class ProGearNavbar extends Component {
                     name="audix"
                     active={activeItem === "audix"}
                     onClick={this.handleItemClick}
-                />
+                >
+                    <Image size="mini" src={VolumeDial} />
+                    Audix
+                </Menu.Item>
                 <Menu.Item
                     as={Link}
                     to="/progear/guytron"
                     name="guytron"
                     active={activeItem === "guytron"}
                     onClick={this.handleItemClick}
-                />
+                >
+                    <Image size="mini" src={VolumeDial} />
+                    Guytron
+                </Menu.Item>
                 <Menu.Item 
                     as={Link}
                     to="/progear/boomerang"
                     name="boomerang"
                     active={activeItem === "boomerang"}
                     onClick={this.handleItemClick}
-                />
+                >
+                    <Image size="mini" src={VolumeDial} />
+                    Boomerang
+                </Menu.Item>
                 <Menu.Item
                     as={Link}
                     to="/progear/radian"
                     name="radian"
                     active={activeItem === "radian"}
                     onClick={this.handleItemClick}
-                />
+                >
+                    <Image size="mini" src={VolumeDial} />
+                    Radian
+                </Menu.Item>
                 <Menu.Item
                     as={Link}
                     to="/progear/georgeLs" 
                     name="georgeLs"
                     active={activeItem === "georgeLs"}
                     onClick={this.handleItemClick}
-                />
+                >
+                    <Image size="mini" src={VolumeDial} />
+                    George L's
+                </Menu.Item>
                 <Menu.Item
                     as={Link}
                     to="/progear/weber"
                     name="weber"
                     active={activeItem === "weber"}
                     onClick={this.handleItemClick}
-                />
+                >
+                    <Image size="mini" src={VolumeDial} />
+                    Weber
+                </Menu.Item>
             </Menu>
         );
     }
