84fa5686ed491b09f4dda75922f449f6b07020a1
James Geib <jpgeib2@gmail.com>
2021-03-13 18:43:54 -0500

    added volume dial bullet point gif to list items in HomeList component

--- client/src/components/HomeList/index.js
+++ client/src/components/HomeList/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { List } from "semantic-ui-react";
+import { List, Image } from "semantic-ui-react";
+import VolumeDial from "../../assets/gifs/dial_bullet.gif";
 
 import "./style.css";
 
@@ -8,12 +9,15 @@ export default class HomeList extends Component {
         return (
             <List>
                 <List.Item className="home-list-item">
+                    <Image src={VolumeDial} size="mini" />
                     <List.Content>Guitar</List.Content>
                 </List.Item>
                 <List.Item className="home-list-item">
+                    <Image src={VolumeDial} size="mini" />
                     <List.Content>Vocals</List.Content>
                 </List.Item>
                 <List.Item className="home-list-item">
+                    <Image src={VolumeDial} size="mini" />
                     <List.Content>Keyboard</List.Content>
                 </List.Item>
             </List>
