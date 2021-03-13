49e38de6ce70a70b33e19be5eb6315b35ae0723b
James Geib <jpgeib2@gmail.com>
2021-03-13 18:34:13 -0500

    applied demo structure for rest of band subsections in LiveMusic page

--- client/src/components/Federation/index.js
+++ client/src/components/Federation/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Grid, Header } from "semantic-ui-react";
+import { Grid, Header, Image } from "semantic-ui-react";
+import PlaceholderImg from "../../assets/images/fl_remys.jpg";
+import { placeholderText } from "../../text";
 import LiveMusicNavbar from "../../containers/LiveMusicNavbar";
 
 import "./style.css";
@@ -8,9 +10,106 @@ class Federation extends Component {
     render() {
         return (
             <>
-                <Grid>
+                 <Grid id="fed-container">
                     <LiveMusicNavbar />
-                    <Header as="h1">Federation</Header>
+                    <Grid.Row>
+                        <Header id="fed-header" as="h1">The Federation</Header>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="medium" src={PlaceholderImg} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h2">[Insert Name Here]</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">{placeholderText}</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="medium" src={PlaceholderImg} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h2">[Insert Name Here]</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">{placeholderText}</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="medium" src={PlaceholderImg} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h2">[Insert Name Here]</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">{placeholderText}</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="medium" src={PlaceholderImg} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h2">[Insert Name Here]</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">{placeholderText}</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="medium" src={PlaceholderImg} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h2">[Insert Name Here]</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">{placeholderText}</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </>
         );
--- client/src/components/ShellShockt/index.js
+++ client/src/components/ShellShockt/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Grid, Header } from "semantic-ui-react";
+import { Grid, Header, Image } from "semantic-ui-react";
+import PlaceholderImg from "../../assets/images/fl_remys.jpg";
+import { placeholderText } from "../../text";
 import LiveMusicNavbar from "../../containers/LiveMusicNavbar";
 
 import "./style.css";
@@ -8,9 +10,106 @@ class ShellShockt extends Component {
     render() {
         return (
             <>
-                <Grid>
+                <Grid id="shell-container">
                     <LiveMusicNavbar />
-                    <Header as="h1">ShellShockt</Header>
+                    <Grid.Row>
+                        <Header id="shell-header" as="h1">ShellShockt</Header>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="medium" src={PlaceholderImg} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h2">[Insert Name Here]</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">{placeholderText}</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="medium" src={PlaceholderImg} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h2">[Insert Name Here]</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">{placeholderText}</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="medium" src={PlaceholderImg} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h2">[Insert Name Here]</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">{placeholderText}</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="medium" src={PlaceholderImg} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h2">[Insert Name Here]</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">{placeholderText}</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="medium" src={PlaceholderImg} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h2">[Insert Name Here]</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">{placeholderText}</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </>
         );
