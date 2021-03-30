162327ffcf33d43844185bf006478ef3462a1bd5
James Geib <jpgeib2@gmail.com>
2021-03-30 11:17:51 -0400

    created bio subcomponents for OutsideChance band page, moved appropriate Grid structure and imports to JeffBio subcomponent and imported into main band page

---
+++ client/src/components/OutsideChance/PegBio/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { Grid, Header, Image } from "semantic-ui-react";
+import PlaceholderImg from "../../../assets/images/fl_remys.jpg";
+import { bios } from "../../../text";
--- client/src/components/OutsideChance/index.js
+++ client/src/components/OutsideChance/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Header, Image } from "semantic-ui-react";
 import PlaceholderImg from "../../assets/images/fl_remys.jpg";
+import JeffBio from "./JeffBio";
 import { bios } from "../../text";
 import BandNavbar from "../../containers/BandNavbar";
 
@@ -16,23 +17,7 @@ class OutsideChance extends Component {
                         <Header id="oc-header" as="h1">Outside Chance</Header>
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image size="medium" src={PlaceholderImg} />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Jeff</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">{bios.oc.jeff}</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
+                        <JeffBio />
                     </Grid.Row>
                     <Grid.Row>
                         <Grid.Column width={8}>
