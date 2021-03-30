29e95d9aa081926b85bd8d4d9f85afd5c48e8d3e
James Geib <jpgeib2@gmail.com>
2021-03-30 11:29:49 -0400

    moved respecitve Grid structures/imports to PegBio, MikeBio, and JimBio subcomponents, then imported all to main band page

--- client/src/components/OutsideChance/PegBio/index.js
+++ client/src/components/OutsideChance/PegBio/index.js
@@ -2,3 +2,27 @@ import React from "react";
 import { Grid, Header, Image } from "semantic-ui-react";
 import PlaceholderImg from "../../../assets/images/fl_remys.jpg";
 import { bios } from "../../../text";
+
+export default () => {
+    return (
+        <>
+            <Grid.Column width={8}>
+                <Image size="medium" src={PlaceholderImg} />
+            </Grid.Column>
+            <Grid.Column width={8}>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Header as="h2">Peg</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Header as="h3">{bios.oc.peg}</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Grid.Column>
+        </>
+    );
+}
--- client/src/components/OutsideChance/index.js
+++ client/src/components/OutsideChance/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { Grid, Header, Image } from "semantic-ui-react";
-import PlaceholderImg from "../../assets/images/fl_remys.jpg";
+import { Grid, Header } from "semantic-ui-react";
 import JeffBio from "./JeffBio";
-import { bios } from "../../text";
+import PegBio from "./PegBio";
+import MikeBio from "./MikeBio";
+import JimBio from "../Federation/JimBio";
 import BandNavbar from "../../containers/BandNavbar";
 
 import "./style.css";
@@ -20,61 +21,13 @@ class OutsideChance extends Component {
                         <JeffBio />
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image size="medium" src={PlaceholderImg} />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Peggy</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">{bios.oc.peg}</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
+                        <PegBio />
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image size="medium" src={PlaceholderImg} />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Mike "Juice"</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">{bios.oc.mike}</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
+                        <MikeBio />
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image size="medium" src={PlaceholderImg} />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Jim</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">{bios.fed.jim}</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
+                        <JimBio />
                     </Grid.Row>
                 </Grid>
             </>
