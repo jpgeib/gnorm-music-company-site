7dafa0b8996aade5b9b9676f24b207e92c96bea9
James Geib <jpgeib2@gmail.com>
2021-03-31 12:33:31 -0400

    embedded video examples of Outside Chance to respective band video page

--- client/src/components/OutsideChanceVideo/index.js
+++ client/src/components/OutsideChanceVideo/index.js
@@ -10,7 +10,25 @@ class OutsideChanceVideo extends Component {
             <>
                 <Grid id="oc-video-container">
                     <BandNavbar />
-                    <Header id="oc-video-header" as="h1">Outside Chance Videos</Header>
+                    <Grid.Row>
+                        <Header id="oc-video-header" as="h1">Outside Chance Videos</Header>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <iframe width="300" height="200" src="https://www.youtube.com/embed/p4Vpk2xuP74" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <iframe width="300" height="200" src="https://www.youtube.com/embed/JKv7CCX9wJc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <iframe width="300" height="200" src="https://www.youtube.com/embed/RgCdaZiXcUY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </>
         );
