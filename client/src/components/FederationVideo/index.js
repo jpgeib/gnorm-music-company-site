ff062ffdf9dab0b07996009fa6ea15f932cebd2e
James Geib <jpgeib2@gmail.com>
2021-03-31 12:06:35 -0400

    embedded video examples of the Federation live performances in FederationVideo page

--- client/src/components/FederationVideo/index.js
+++ client/src/components/FederationVideo/index.js
@@ -10,7 +10,27 @@ class FederationVideo extends Component {
             <>
                 <Grid id="fed-video-container">
                     <BandNavbar />
-                    <Header id="fed-video-header" as="h1">The Federation Videos</Header>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Header id="fed-video-header" as="h1">The Federation Videos</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <iframe id="miss-queen" src="https://www.youtube.com/embed/NAE10R4y1_k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <iframe id="ziggy-star" src="https://www.youtube.com/embed/EJ7lu1SHmVo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <iframe id="flirtin" src="https://www.youtube.com/embed/3g-eqjd8l10" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <iframe id="cant-help" src="https://www.youtube.com/embed/Eo9zzy0rZEg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </>
         );
