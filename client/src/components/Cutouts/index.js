2bedee8cbf73da53a6db5980fee1d1650cc5b58c
James Geib <jpgeib2@gmail.com>
2021-03-30 23:02:26 -0400

    built preliminary subcomponents for Cutouts band page, passed down text props

--- client/src/components/Cutouts/index.js
+++ client/src/components/Cutouts/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-import { Grid, Header, Image } from "semantic-ui-react";
-import PlaceholderImg from "../../assets/images/fl_remys.jpg";
-import { placeholderText } from "../../text";
+import { Grid, Header } from "semantic-ui-react";
+import { bios } from "../../text";
 import BandNavbar from "../../containers/BandNavbar";
+import DariaBio from "./DariaBio";
+import DaveBio from "./DaveBio";
+import JimBio from "../Federation/JimBio";
 
 import "./style.css";
+
 
 class Cutouts extends Component {
     render() {
@@ -16,61 +19,13 @@ class Cutouts extends Component {
                         <Header id="cut-header" as="h1">The Cutouts</Header>
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image size="medium" src={PlaceholderImg} />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Daria</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">{placeholderText}</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
+                        <DariaBio daria={bios.cut.daria} />
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image size="medium" src={PlaceholderImg} />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Dave</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">{placeholderText}</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
+                        <DaveBio dave={bios.cut.dave} />
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image size="medium" src={PlaceholderImg} />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Jim</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">{placeholderText}</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
+                        <JimBio jim={bios.fed.jim} />
                     </Grid.Row>
                 </Grid>
             </>
