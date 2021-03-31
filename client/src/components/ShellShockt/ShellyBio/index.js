a25b2a29f6d75feeaf0919cdf686e9a0b60732e3
James Geib <jpgeib2@gmail.com>
2021-03-30 20:44:48 -0400

    constructed Shellshockt band member subcomponents and passed down text valuesthrough props

---
+++ client/src/components/ShellShockt/ShellyBio/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Grid, Header, Image } from "semantic-ui-react";
+import PlaceholderImg from "../../../assets/images/fl_remys.jpg";
+
+export default (props) => {
+    return (
+        <>
+            <Grid.Column width={8}>
+                <Image size="medium" src={PlaceholderImg} />
+            </Grid.Column>
+            <Grid.Column width={8}>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Header as="h2">Shelly</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Header as="h3">{props.shelly}</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Grid.Column>
+        </>
+    );
+}
--- client/src/components/ShellShockt/index.js
+++ client/src/components/ShellShockt/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import { Grid, Header, Image } from "semantic-ui-react";
-import PlaceholderImg from "../../assets/images/fl_remys.jpg";
-import { placeholderText } from "../../text";
+import { Grid, Header } from "semantic-ui-react";
+import { bios } from "../../text";
 import BandNavbar from "../../containers/BandNavbar";
+import ShellyBio from "./ShellyBio";
+import RonBio from "./RonBio";
+import HansBio from "./HansBio";
+import JayBio from "./JayBio";
 
 import "./style.css";
 
@@ -16,80 +19,16 @@ class ShellShockt extends Component {
                         <Header id="shell-header" as="h1">ShellShockt</Header>
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image size="medium" src={PlaceholderImg} />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Shelly</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">{placeholderText}</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
+                        <ShellyBio shelly={bios.shell.shelly} />
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image size="medium" src={PlaceholderImg} />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Ron</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">{placeholderText}</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
+                        <RonBio ron={bios.shell.ron} />
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image size="medium" src={PlaceholderImg} />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Hans</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">{placeholderText}</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
+                        <HansBio hans={bios.shell.hans} />
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image size="medium" src={PlaceholderImg} />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Jay</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">{placeholderText}</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
+                        <JayBio jay={bios.shell.jay} />
                     </Grid.Row>
                 </Grid>
             </>
