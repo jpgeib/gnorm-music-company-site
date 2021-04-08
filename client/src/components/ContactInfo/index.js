83d9c1c26a52b0e3f4e6c75575d5896832f2332a
James Geib <jpgeib2@gmail.com>
2021-04-08 16:52:49 -0400

    deleted unnecessary id attributes from ContactInfo

--- client/src/components/ContactInfo/index.js
+++ client/src/components/ContactInfo/index.js
@@ -4,12 +4,12 @@ import { Header } from "semantic-ui-react";
 export default (props) => {
     return (
         <>
-            <Header id="tagline" as="h3">Gnorm (The 'G' is silent, I'm not...)</Header>
+            <Header as="h3">Gnorm (The 'G' is silent, I'm not...)</Header>
             <Header style={props.contactInfo.header} as="h3">Gnorm Music, LLC</Header>
             <Header style={props.contactInfo.header} as="h3">PO Box 5152</Header>
-            <Header id="city" as="h3">Milford, CT 06460</Header>
-            <Header id="phone" as="h3">Phone: +1 (203) 605-8665</Header>
-            <Header id="email" as="h3">Email: <a style={props.contactInfo.emailLink} href="mailto:gnorm@gnormmusic.com">gnorm@gnormmusic.com</a></Header>
+            <Header as="h3">Milford, CT 06460</Header>
+            <Header as="h3">Phone: +1 (203) 605-8665</Header>
+            <Header as="h3">Email: <a style={props.contactInfo.emailLink} href="mailto:gnorm@gnormmusic.com">gnorm@gnormmusic.com</a></Header>
         </>
     );
 }
