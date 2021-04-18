c62be03c5193fe8ea63aab88df02d1f36860496c
James Geib <jpgeib2@gmail.com>
2021-04-18 15:52:49 -0400

    replaced old text for JimBio component

--- client/src/text/index.js
+++ client/src/text/index.js
@@ -44,7 +44,7 @@ export const bios = {
     },
     fed: {
         joel: "This will be where Joel's bio is placed once it is made.",
-        jim: "Jim has been playing music for his entire life, providing a strong and powerful backbeat along with a technical ability developed at a young age. His great sense of musicality is also prevalent in his playing as well because of his exposure to many different genres that he listened to growing up. He credits the British Invasion and pop music from the early sixties as his first exposure to music, however, he was also influenced by jazz and big band music from his father, who also was a drummer."
+        jim: "Jim has been playing drums since he [was knee high to a grasshopper] encouraged by his drummer dad, who introduced Jim to jazz and Big Band music. At age 8, Jim discovered Led Zeppelin and Frank Zappa, and at 9 he sat in with his drum teacher's band performing at a club and was hooked. By 15, Jim was playing in local clubs (with his dad as roadie) and has regularly performed around the tri-state area [music scene] ever since. Jim is the 'go-to' drummer for many local bands, including Outside Chance, the Federation and the Cut-Outs. He also plays guitar, keyboards and electronic music."
     },
     shell: {
         shelly: "This will be where Shelly's bio is placed once it is made.",
