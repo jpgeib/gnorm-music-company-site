c06f0f5827757666f610faa9c2928a8fb6abbd54
James Geib <jpgeib2@gmail.com>
2021-04-19 21:15:54 -0400

    applied same changes to ProGearRoutes

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -4,17 +4,10 @@ import Navbar from "../Navbar";
 import LandingPage from '../../pages/LandingPage';
 import Home from "../../pages/Home";
 import LiveMusicRoutes from "../../routes/LiveMusicRoutes";
-import ProGear from "../../pages/ProGear";
+import ProGearRoutes from "../../routes/ProGearRoutes";
 import Shows from "../../pages/Shows";
 import Contact from "../../pages/Contact";
 import Footer from "../../components/Footer";
-import Audix from "../../components/Audix";
-import Guytron from "../../components/Guytron";
-import BoomerangMusic from "../../components/BoomerangMusic";
-import RadianAudio from "../../components/RadianAudio";
-import GeorgeLs from "../../components/GeorgeLs";
-import WeberVST from "../../components/WeberVST";
-
 import { subNavStyle, contactInfoStyle, progearEntryStyle, bandPageStyle } from "../../styles";
 import "./style.css";
 
@@ -27,13 +20,7 @@ class App extends Component {
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/home" component={Home} />
         <LiveMusicRoutes subNav={subNavStyle} band={bandPageStyle} />
-        <Route exact path="/progear" render={ () => <ProGear subNav={subNavStyle} /> } />
-        <Route exact path="/progear/audix" render={ () => <Audix entry={progearEntryStyle} subNav={subNavStyle} /> } />
-        <Route exact path="/progear/guytron" render={ () => <Guytron entry={progearEntryStyle} subNav={subNavStyle} /> } />
-        <Route exact path="/progear/boomerang" render={ () => <BoomerangMusic entry={progearEntryStyle} subNav={subNavStyle} /> } />
-        <Route exact path="/progear/radian" render={ () => <RadianAudio entry={progearEntryStyle} subNav={subNavStyle} /> } />
-        <Route exact path="/progear/georgeLs" render={ () => <GeorgeLs entry={progearEntryStyle} subNav={subNavStyle} /> } />
-        <Route exact path="/progear/weber" render={ () => <WeberVST entry={progearEntryStyle} subNav={subNavStyle} /> } />
+        <ProGearRoutes entry={progearEntryStyle} subNav={subNavStyle} />
         <Route exact path="/shows" component={Shows} />
         <Route exact path="/contact" render={ () => <Contact contactInfo={contactInfoStyle} /> } />
         {this.props.history.location.pathname !== "/" && <Footer />}
--- client/src/routes/ProGearRoutes/index.js
+++ client/src/routes/ProGearRoutes/index.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import ProGear from "../../pages/ProGear";
+import Audix from "../../components/Audix";
+import Guytron from "../../components/Guytron";
+import BoomerangMusic from "../../components/BoomerangMusic";
+import RadianAudio from "../../components/RadianAudio";
+import GeorgeLs from "../../components/GeorgeLs";
+import WeberVST from "../../components/WeberVST";
 
 export default class ProGearRoutes extends Component {
     render() {
         return (
             <>
-                
+                <Route exact path="/progear" render={() => <ProGear subNav={this.props.subNav} />} />
+                <Route exact path="/progear/audix" render={() => <Audix entry={this.props.entry} subNav={this.props.subNav} />} />
+                <Route exact path="/progear/guytron" render={() => <Guytron entry={this.props.entry} subNav={this.props.subNav} />} />
+                <Route exact path="/progear/boomerang" render={() => <BoomerangMusic entry={this.props.entry} subNav={this.props.subNav} />} />
+                <Route exact path="/progear/radian" render={() => <RadianAudio entry={this.props.entry} subNav={this.props.subNav} />} />
+                <Route exact path="/progear/georgeLs" render={() => <GeorgeLs entry={this.props.entry} subNav={this.props.subNav} />} />
+                <Route exact path="/progear/weber" render={() => <WeberVST entry={this.props.entry} subNav={this.props.subNav} />} />
             </>
         )
     }
