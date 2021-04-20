f049b11fa0e12487c3387abe2e433fba0f2954d5
James Geib <jpgeib2@gmail.com>
2021-04-20 11:03:39 -0400

    minor adjustments

--- client/src/pages/LiveMusic/index.js
+++ client/src/pages/LiveMusic/index.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import { Grid, Image, Header } from "semantic-ui-react";
 import { livemusicText } from "../../text";
 import OutsideChanceImage from "../../assets/images/336_OutsideChance1.JPG";
-import LiveMusicNavbar from "../../containers/LiveMusicNavbar";
-
-import "./style.css";
 
 class LiveMusic extends Component {
     render() {
