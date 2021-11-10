6ecd5c66df008a7125c88c21dba31f69c3098a66
James Geib <jpgeib2@gmail.com>
2021-11-10 11:18:08 -0500

    minor change

--- client/src/components/ShowDates/AlfaPizza/index.js
+++ client/src/components/ShowDates/AlfaPizza/index.js
@@ -13,7 +13,7 @@ export default (props) => {
                 <Image size="big" src={AlfaPizzaImage} />
             </Grid.Column>
             <Grid.Column width={8}>
-                <Header style={header} as="h1">Alfa Pizza - Milford, CT</Header>
+                <Header style={header} as="h1">Alfa's Pizza - Milford, CT</Header>
                 <Header style={header} as="h3"><Image src={VolumeDial} />The Void</Header>
                 <Header style={header} as="h3"><Image src={VolumeDial} />Saturday 11/6 - 8PM-11PM </Header>
             </Grid.Column>
